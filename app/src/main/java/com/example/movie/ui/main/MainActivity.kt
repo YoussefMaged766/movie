26936e83ba17b7431865e48642b5d8be13efcb93
YoussefMaged766 <yoer766@gmail.com>
2022-08-23 21:54:26 +0200

    category

--- app/src/main/java/com/example/movie/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/movie/ui/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.movie.ui.main
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
@@ -9,13 +11,15 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movie.*
+import com.example.movie.R
 import com.example.movie.adapter.adapter
 import com.example.movie.databinding.ActivityMainBinding
 import com.example.movie.models.movie
@@ -23,20 +27,41 @@ import com.example.movie.util.CenterZoomLayoutManager
 
 class MainActivity : AppCompatActivity() {
 lateinit var binding:ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 //      binding.bottomNavigationView.setupWithNavController(navController)
         NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
+
+        binding.toolbar.getNavigationIcon()
+            ?.setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_trend,R.id.nav_tv
+            )
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+
+
 
 
 
 
 
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 
 
 
